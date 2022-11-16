cfe574638f579f978a7ebbab5de7a1ef0925cbe9
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-11-16 12:55:39 -0400

    style: Formatted with prettier

--- servidor/src/index.js
+++ servidor/src/index.js
@@ -1,32 +1,34 @@
 // JWT
-require("dotenv-safe").config();
+require("dotenv/config");
 const { expressjwt: expressJWT } = require("express-jwt");
-const cors = require('cors');
-const { routerAuth } = require("./routes/Auth")
-const cookieParser = require('cookie-parser')
-const { routerUser } = require("./routes/User")
-const express = require('express');
+const cors = require("cors");
+const { routerAuth } = require("./routes/Auth");
+const cookieParser = require("cookie-parser");
+const { routerUser } = require("./routes/User");
+const express = require("express");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(cookieParser());
 app.use(
   expressJWT({
     secret: process.env.SECRET,
     algorithms: ["HS256"],
-    getToken: req => req.cookies.token
-  }).unless({ path: ["/autenticar", "/logar", "/deslogar"] })
+    getToken: (req) => req.cookies.token,
+  }).unless({
+    path: ["/autenticar", "/logar", "/deslogar", "/create", "/user"],
+  })
 );
 app.use(routerAuth);
 app.use(routerUser);
 app.listen(3000, function () {
-  console.log('App de Exemplo escutando na porta 3000!')
+  console.log("App de Exemplo escutando na porta 3000!");
 });
