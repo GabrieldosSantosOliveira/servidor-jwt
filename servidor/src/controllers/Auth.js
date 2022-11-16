a0a9ad0ca7edf183c7d63c3a68ea93b5051187fe
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-11-16 12:56:57 -0400

    feat: Using decrypted password to authenticate

--- servidor/src/controllers/Auth.js
+++ servidor/src/controllers/Auth.js
@@ -1,18 +1,31 @@
 // JWT
-require("dotenv-safe").config();
+require('dotenv-safe').config();
 const jwt = require('jsonwebtoken');
+const { decrypt } = require('./../utils/decrypt');
 const { usuario } = require('./../../models');
 class Auth {
   static async authentication(req, res) {
     res.render('autenticar');
   }
   static async home(req, res) {
-    res.render("home")
+    res.render('home');
   }
   static async login(req, res) {
     const { user, password } = req.body;
     const data = await usuario.findOne({ where: { user } });
-    if (user === data.user && password === data.password) {
+    if (!data) {
+      res
+        .status(401)
+        .json({ message: 'Usuário não encontrado!' });
+    }
+    console.log(
+      decrypt('6a18143a62c306392fedd364d7c2aac9')
+    );
+    console.log(decrypt(data.password));
+    if (
+      user === data.user &&
+      password === decrypt(data.password)
+    ) {
       const id = 1;
       const token = jwt.sign({ id }, process.env.SECRET, {
         expiresIn: 300 // expires in 5min
@@ -26,7 +39,7 @@ class Auth {
   }
   static async logout(req, res) {
     res.cookie('token', null, { httpOnly: true });
-    res.json({ deslogado: true })
+    res.json({ deslogado: true });
   }
 }
 
