fa2ca710adb8965356d6a219d001cbba7689309e
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-11-18 10:57:02 -0400

    fix: Add settings

--- servidor/src/utils/encrypt.js
+++ servidor/src/utils/encrypt.js
@@ -2,16 +2,12 @@ const { createCipheriv } = require('crypto');
 require('dotenv/config');
 const encrypt = text => {
   const cipher = createCipheriv(
-    process.env.ALGORITHM_HASH,
-    process.env.PASSWORD_HASH,
-    process.env.IV_HASH
+    'aes-256-cbc',
+    'bf3c199c2470cb477d907b1e0917c17e',
+    '5183666c72eec9e4'
   );
-  let encrypted = cipher.update(
-    text,
-    'utf8',
-    process.env.METHOD_HASH
-  );
-  encrypted += cipher.final(process.env.METHOD_HASH);
+  let encrypted = cipher.update(text, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
   return encrypted;
 };
 module.exports = { encrypt };
