2b9fb0fd1daf273df02cee441fcda3d55dbf5291
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-11-18 10:56:53 -0400

    fix: Add settings

--- servidor/src/utils/decrypt.js
+++ servidor/src/utils/decrypt.js
@@ -1,16 +1,11 @@
 const { createDecipheriv } = require('crypto');
-require('dotenv/config');
 const decrypt = text => {
   let decipher = createDecipheriv(
-    process.env.ALGORITHM_HASH,
-    process.env.PASSWORD_HASH,
-    process.env.IV_HASH
+    'aes-256-cbc',
+    'bf3c199c2470cb477d907b1e0917c17e',
+    '5183666c72eec9e4'
   );
-  let decrypted = decipher.update(
-    text,
-    process.env.METHOD_HASH,
-    'utf8'
-  );
+  let decrypted = decipher.update(text, 'hex', 'utf8');
   return decrypted + decipher.final('utf8');
 };
 module.exports = { decrypt };
